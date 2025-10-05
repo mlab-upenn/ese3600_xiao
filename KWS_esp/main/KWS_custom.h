c902dde29ac441b22239a7c4c36abe2021f8726b
Aidan McGoogan <aidanmcgoogan@Aidans-MacBook-Pro.local>
2025-10-05 12:59:42 -0400

    kws lab incomplete

---
+++ KWS_esp/main/KWS_custom.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Defined in KWS_custom.cc
+extern unsigned char g_model[];
+extern unsigned int  g_model_len;
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
